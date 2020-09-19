c5580775afe2bad91e49083a80629da043258dd5
Tangui <29804907+tanguilp@users.noreply.github.com>
2020-09-19 10:56:54 +0300

    Add package info

--- mix.exs
+++ mix.exs
@@ -4,11 +4,14 @@ defmodule WaxAPIREST.MixProject do
   def project do
     [
       app: :wax_api_rest,
+      description: "FIDO2 / WebAuthn server REST API library",
       version: "0.2.0",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
-      docs: [main: "readme", extras: ["README.md"]]
+      docs: [main: "readme", extras: ["README.md"]],
+      package: package(),
+      source_url: "https://github.com/tanguilp/wax_api_rest"
     ]
   end
 
@@ -26,6 +29,13 @@ defmodule WaxAPIREST.MixProject do
       {:ex_doc, "~> 0.19", only: :dev, runtime: false},
       {:plug, "~> 1.0"},
       {:wax_, "~> 0.3.0"}
+    ]
+  end
+
+  def package() do
+    [
+      licenses: ["Apache-2.0"],
+      links: %{"GitHub" => "https://github.com/tanguilp/wax_api_rest"}
     ]
   end
 end
