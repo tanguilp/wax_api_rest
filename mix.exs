852ac181afaaeb6b66796fb0ba3d0b4c347992bc
Tangui Le Pense <29804907+tanguilp@users.noreply.github.com>
2022-11-04 23:55:50 +0300

    Update wax dependency to 0.5.0

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule WaxAPIREST.MixProject do
     [
       app: :wax_api_rest,
       description: "FIDO2 / WebAuthn server REST API library",
-      version: "0.2.0",
+      version: "0.3.0",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
@@ -28,7 +28,7 @@ defmodule WaxAPIREST.MixProject do
       {:dialyxir, "~> 1.0.0-rc.4", only: [:dev], runtime: false},
       {:ex_doc, "~> 0.19", only: :dev, runtime: false},
       {:plug, "~> 1.0"},
-      {:wax_, "~> 0.3.0"}
+      {:wax_, "~> 0.5.0"}
     ]
   end
 
